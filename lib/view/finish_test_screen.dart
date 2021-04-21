ede0c159029139871ea00bfa6bc6b236de59897a
hasan-ciftci <hasan.ciftci@ankabeta.com>
2021-04-21 18:01:46 +0300

    Qustion view mock functionality added.

---
+++ lib/view/finish_test_screen.dart
@@ -0,0 +1,122 @@
+import 'package:dodi/core/constants/image_constants.dart';
+import 'package:flutter/material.dart';
+
+class FinishTestView extends StatefulWidget {
+  @override
+  _FinishTestViewState createState() => _FinishTestViewState();
+}
+
+class _FinishTestViewState extends State<FinishTestView> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return Stack(
+      children: [
+        buildBody(size, context),
+      ],
+    );
+  }
+
+  Align buildPageHeader(Size size) {
+    return Align(
+      alignment: Alignment.topCenter,
+      child: Container(
+        height: size.height * .125,
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Icon(Icons.arrow_back_ios_outlined),
+              Text("1. Sınıf"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Align buildBody(Size size, BuildContext context) {
+    return Align(
+      alignment: Alignment.center,
+      child: Container(
+        height: size.height * .75,
+        width: size.width,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+              bottomRight: Radius.circular(50),
+              bottomLeft: Radius.circular(50)),
+          color: Colors.white,
+        ),
+        child: Column(
+          children: [
+            buildTestIntroductionImage(size),
+            buildTestIntroductionText(size, context),
+            buildStartTestButton(size, context)
+          ],
+        ),
+      ),
+    );
+  }
+
+  Image buildTestIntroductionImage(Size size) {
+    return Image.asset(
+      ImageConstants.instance.getClassImage(0),
+      height: size.height * .4,
+      width: size.width * .4,
+      fit: BoxFit.contain,
+    );
+  }
+
+  SizedBox buildTestIntroductionText(Size size, BuildContext context) {
+    return SizedBox(
+      height: size.height * .2,
+      child: Text(
+        "Sınıflar için Görsel Algı ve Dikkat Testi",
+        style: Theme.of(context).textTheme.subtitle1,
+      ),
+    );
+  }
+
+  SizedBox buildStartTestButton(Size size, BuildContext context) {
+    return SizedBox(
+      height: size.height * .15,
+      child: Center(
+        child: SizedBox(
+          height: size.height * .08,
+          width: size.width * .9,
+          child: ElevatedButton(
+            onPressed: () {},
+            child: Text(
+              "Testi bitir",
+              style: Theme.of(context).textTheme.headline5,
+            ),
+            style: ButtonStyle(
+              shape: MaterialStateProperty.all(StadiumBorder()),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Container buildBackground(Size size, BuildContext context) {
+    return Container(
+      height: size.height,
+      width: size.width,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          stops: [0.0, 0.5],
+          colors: [
+            Theme.of(context).backgroundColor,
+            Theme.of(context).primaryColor,
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/view/question_view.dart
+++ lib/view/question_view.dart
@@ -1,14 +1,44 @@
 import 'package:dodi/core/constants/image_constants.dart';
 import 'package:flutter/material.dart';
 
+import 'finish_test_screen.dart';
+
 class QuestionView extends StatefulWidget {
   @override
   _QuestionViewState createState() => _QuestionViewState();
 }
 
-class _QuestionViewState extends State<QuestionView> {
+class _QuestionViewState extends State<QuestionView>
+    with SingleTickerProviderStateMixin {
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 4, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
+    List<QuestionModel> questions = [
+      QuestionModel(
+        questionImage: ImageConstants.instance.soru1,
+        answerImage: ImageConstants.instance.cevap1,
+        text: 'Yukarıda verilen şeklin kapalı hali aşağıdakilerden hangisidir?',
+      ),
+      QuestionModel(
+        questionImage: ImageConstants.instance.soru2,
+        answerImage: ImageConstants.instance.cevap2,
+        text:
+            'Yönerge 1: Kırmızı üçgenler sonda değildir. Yönerge 2: Sarı üçgen kırmızı üçgenlerin solundadır.\n\nYukarıda verilen yönergelere göre üçgenlerin doğru sıralanışı aşağıdakilerden hangisidir?',
+      ),
+      QuestionModel(
+        questionImage: ImageConstants.instance.soru3,
+        answerImage: ImageConstants.instance.cevap3,
+        text:
+            'Yukarıda verilen sıralamaya göre soru işareti yerine aşağıdakilerden hangisi gelmelidir?',
+      ),
+    ];
     Size size = MediaQuery.of(context).size;
 
     return Scaffold(
@@ -16,66 +46,94 @@ class _QuestionViewState extends State<QuestionView> {
         fit: StackFit.loose,
         children: [
           buildBackground(size, context),
-          buildPageHeader(size),
-          Align(
-            alignment: Alignment.center,
-            child: Container(
-              height: size.height * .75,
-              width: size.width,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(50),
-                  bottomRight: Radius.circular(50),
+          buildPageHeader(size, _tabController),
+          TabBarView(controller: _tabController, children: [
+            Stack(
+              children: [
+                Align(
+                  alignment: Alignment.center,
+                  child: Container(
+                    height: size.height * .75,
+                    width: size.width,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(50),
+                        bottomRight: Radius.circular(50),
+                      ),
+                      color: Colors.white,
+                    ),
+                    child: QuestionCard(
+                      size: size,
+                      questionText: questions[0].text,
+                      questionAnswer: questions[0].answerImage,
+                      questionImage: questions[0].questionImage,
+                    ),
+                  ),
                 ),
-                color: Colors.white,
-              ),
-              child: Column(
-                children: [
-                  buildQuestionImage(),
-                  buildQuestionText(size, context),
-                  buildAnswer(),
-                ],
-              ),
+                Options(size: size),
+              ],
             ),
-          ),
-          buildPageFooter(size),
+            Stack(
+              children: [
+                Align(
+                  alignment: Alignment.center,
+                  child: Container(
+                    height: size.height * .75,
+                    width: size.width,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(50),
+                        bottomRight: Radius.circular(50),
+                      ),
+                      color: Colors.white,
+                    ),
+                    child: QuestionCard(
+                      size: size,
+                      questionText: questions[1].text,
+                      questionAnswer: questions[1].answerImage,
+                      questionImage: questions[1].questionImage,
+                    ),
+                  ),
+                ),
+                Options(size: size),
+              ],
+            ),
+            Stack(
+              children: [
+                Align(
+                  alignment: Alignment.center,
+                  child: Container(
+                    height: size.height * .75,
+                    width: size.width,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(50),
+                        bottomRight: Radius.circular(50),
+                      ),
+                      color: Colors.white,
+                    ),
+                    child: QuestionCard(
+                      size: size,
+                      questionText: questions[2].text,
+                      questionAnswer: questions[2].answerImage,
+                      questionImage: questions[2].questionImage,
+                    ),
+                  ),
+                ),
+                Options(size: size),
+              ],
+            ),
+            FinishTestView(),
+          ]),
         ],
       ),
     );
   }
 
-  Expanded buildQuestionImage() {
-    return Expanded(
-      child: Image.asset(ImageConstants.instance.soru1),
-    );
-  }
-
-  Expanded buildQuestionText(Size size, BuildContext context) {
-    return Expanded(
-      child: Padding(
-        padding: EdgeInsets.symmetric(
-          horizontal: size.width * .1,
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: SingleChildScrollView(
-            child: Text(
-              "Yönerge 1: Kırmızı üçgenler sonda değildir.\nYönerge 2: Sarı üçgen kırımızı üçgenlerin solundadır\n\nYukarıda verilen yönergelere göre üçgenlerin doğru sıralanışı aşağıdakilerden hangisidir?",
-              style: Theme.of(context).textTheme.subtitle1,
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  Expanded buildAnswer() {
-    return Expanded(
-      child: Image.asset(ImageConstants.instance.cevap1),
-    );
-  }
-
-  Align buildPageHeader(Size size) {
+  Align buildPageHeader(Size size, TabController tabController) {
+    tabController.addListener(() {
+      setState(() {});
+    });
     return Align(
       alignment: Alignment.topCenter,
       child: Container(
@@ -101,10 +159,16 @@ class _QuestionViewState extends State<QuestionView> {
                     height: size.height * .01,
                     color: Theme.of(context).disabledColor,
                   ),
-                  Container(
-                    width: size.width * .5,
-                    height: size.height * .01,
-                    color: Theme.of(context).primaryColor,
+                  AnimatedContainer(
+                    duration: Duration(milliseconds: 1000),
+                    curve: Curves.elasticInOut,
+                    child: Container(
+                      width: size.width *
+                          (tabController.index + 1) /
+                          (tabController.length),
+                      height: size.height * .01,
+                      color: Theme.of(context).primaryColor,
+                    ),
                   ),
                 ],
               ),
@@ -132,35 +196,6 @@ class _QuestionViewState extends State<QuestionView> {
       ),
     );
   }
-
-  Align buildPageFooter(Size size) {
-    return Align(
-      alignment: Alignment.bottomCenter,
-      child: Container(
-        height: size.height * .125,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Option(
-              color: Theme.of(context).primaryColor,
-              size: size,
-              option: "A",
-            ),
-            Option(
-              color: Theme.of(context).primaryColor,
-              size: size,
-              option: "B",
-            ),
-            Option(
-              color: Theme.of(context).primaryColor,
-              size: size,
-              option: "C",
-            ),
-          ],
-        ),
-      ),
-    );
-  }
 }
 
 class Option extends StatelessWidget {
@@ -177,23 +212,205 @@ class Option extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => print(option),
-      child: Container(
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          color: Colors.white,
+    print("rebuild");
+    print(color.toString());
+    return Container(
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: Colors.white,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(2),
+        child: CircleAvatar(
+          backgroundColor: color,
+          radius: size.height * .045,
+          child: Text(
+            option,
+            style: Theme.of(context).textTheme.headline4,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuestionCard extends StatefulWidget {
+  final String questionImage;
+  final String questionAnswer;
+  final String questionText;
+  final Size size;
+
+  const QuestionCard(
+      {Key key,
+      @required this.questionImage,
+      @required this.questionAnswer,
+      @required this.questionText,
+      @required this.size})
+      : super(key: key);
+
+  @override
+  _QuestionCardState createState() => _QuestionCardState();
+}
+
+class _QuestionCardState extends State<QuestionCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        buildQuestionImage(),
+        buildQuestionText(widget.size, context),
+        buildAnswer(),
+      ],
+    );
+  }
+
+  Expanded buildQuestionImage() {
+    return Expanded(
+      child: Image.asset(widget.questionImage),
+    );
+  }
+
+  Expanded buildQuestionText(Size size, BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: size.width * .1,
         ),
         child: Padding(
-          padding: const EdgeInsets.all(2),
-          child: CircleAvatar(
-            backgroundColor: Theme.of(context).primaryColor,
-            radius: size.height * .045,
-            child: Text(
-              option,
-              style: Theme.of(context).textTheme.headline4,
+          padding: const EdgeInsets.all(8.0),
+          child: Center(
+            child: SingleChildScrollView(
+              child: Text(
+                widget.questionText,
+                style: Theme.of(context).textTheme.subtitle1,
+              ),
             ),
           ),
+        ),
+      ),
+    );
+  }
+
+  Expanded buildAnswer() {
+    return Expanded(
+      child: Image.asset(widget.questionAnswer),
+    );
+  }
+}
+
+class QuestionModel {
+  final String questionImage;
+  final String answerImage;
+  final String text;
+
+  QuestionModel(
+      {@required this.questionImage,
+      @required this.answerImage,
+      @required this.text});
+}
+
+enum SelectedOption { A, B, C }
+
+class Options extends StatefulWidget {
+  final Size size;
+
+  const Options({Key key, this.size}) : super(key: key);
+
+  @override
+  _OptionsState createState() => _OptionsState();
+}
+
+class _OptionsState extends State<Options> {
+  SelectedOption selectedOption;
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        height: widget.size.height * .125,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  selectedOption = SelectedOption.A;
+                });
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.white,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(2),
+                  child: CircleAvatar(
+                    backgroundColor: selectedOption == SelectedOption.A
+                        ? Theme.of(context).backgroundColor
+                        : Theme.of(context).primaryColor,
+                    radius: widget.size.height * .045,
+                    child: Text(
+                      "A",
+                      style: Theme.of(context).textTheme.headline4,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  selectedOption = SelectedOption.B;
+                });
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.white,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(2),
+                  child: CircleAvatar(
+                    backgroundColor: selectedOption == SelectedOption.B
+                        ? Theme.of(context).backgroundColor
+                        : Theme.of(context).primaryColor,
+                    radius: widget.size.height * .045,
+                    child: Text(
+                      "B",
+                      style: Theme.of(context).textTheme.headline4,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  selectedOption = SelectedOption.C;
+                });
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.white,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(2),
+                  child: CircleAvatar(
+                    backgroundColor: selectedOption == SelectedOption.C
+                        ? Theme.of(context).backgroundColor
+                        : Theme.of(context).primaryColor,
+                    radius: widget.size.height * .045,
+                    child: Text(
+                      "C",
+                      style: Theme.of(context).textTheme.headline4,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
