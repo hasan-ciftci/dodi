10f47cea4d565da8549b915cef6a15b4291e60e3
hasan çiftçi <hasanciftci@hasan-MacBook-Pro.local>
2021-05-18 15:59:16 +0300

    Test introduction screen, test details text added with animation.

--- lib/view/test_introduction_view.dart
+++ lib/view/test_introduction_view.dart
@@ -8,6 +8,8 @@ class TestIntroductionView extends StatefulWidget {
 }
 
 class _TestIntroductionViewState extends State<TestIntroductionView> {
+  bool isDetailShowed = false;
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -15,9 +17,10 @@ class _TestIntroductionViewState extends State<TestIntroductionView> {
     return Scaffold(
       body: Stack(
         children: [
-          buildBackground(size, context),
+          buildBackground(size),
           buildPageHeader(size),
-          buildBody(size, context),
+          buildBody(size),
+          buildFooter(size)
         ],
       ),
     );
@@ -49,21 +52,47 @@ class _TestIntroductionViewState extends State<TestIntroductionView> {
     );
   }
 
-  Align buildBody(Size size, BuildContext context) {
+  Align buildBody(Size size) {
     return Align(
-      alignment: Alignment.center,
-      child: Container(
-        height: size.height * .75,
+      alignment: Alignment.topCenter,
+      child: AnimatedContainer(
+        margin: EdgeInsets.only(
+          top: size.height * .125,
+        ),
+        height: isDetailShowed ? size.height * .6 : size.height * .75,
         width: size.width,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(50.0),
           color: Colors.white,
         ),
+        duration: Duration(milliseconds: 250),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            buildTestIntroductionImage(size),
-            buildTestIntroductionText(size, context),
-            buildStartTestButton(size, context)
+            Column(
+              children: [
+                buildTestIntroductionImage(size),
+                buildTestIntroductionText(size, context),
+                buildStartTestButton(size, context),
+              ],
+            ),
+            Visibility(
+              visible: isDetailShowed ? false : true,
+              child: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    isDetailShowed = true;
+                  });
+                },
+                child: Container(
+                  color: Colors.transparent,
+                  child: Padding(
+                    padding: EdgeInsets.only(bottom: 16.0),
+                    child: Text("Test Detayları"),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
@@ -73,42 +102,76 @@ class _TestIntroductionViewState extends State<TestIntroductionView> {
   Image buildTestIntroductionImage(Size size) {
     return Image.asset(
       ImageConstants.instance.getClassImage(0),
-      height: size.height * .4,
-      width: size.width * .4,
+      height: size.height * .35,
+      width: size.width * .35,
       fit: BoxFit.contain,
     );
   }
 
-  SizedBox buildTestIntroductionText(Size size, BuildContext context) {
-    return SizedBox(
-      height: size.height * .2,
-      child: Text(
-        "Sınıflar için Görsel Algı ve Dikkat Testi",
-        style: Theme.of(context).textTheme.subtitle1,
+  Text buildTestIntroductionText(Size size, BuildContext context) {
+    return Text(
+      "1. Sınıflar için Görsel Algı ve Dikkat Testi",
+      textAlign: TextAlign.center,
+      style: Theme.of(context).textTheme.subtitle1,
+    );
+  }
+
+  Padding buildStartTestButton(Size size, BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: SizedBox(
+        height: size.height * .08,
+        width: size.width * .9,
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pushNamed('/quizPage');
+          },
+          child: Text(
+            "Teste Başla",
+            style: Theme.of(context)
+                .textTheme
+                .headline5
+                .copyWith(color: Colors.white),
+          ),
+          style: ButtonStyle(
+            shape: MaterialStateProperty.all(StadiumBorder()),
+          ),
+        ),
       ),
     );
   }
 
-  SizedBox buildStartTestButton(Size size, BuildContext context) {
-    return SizedBox(
-      height: size.height * .15,
-      child: Center(
-        child: SizedBox(
-          height: size.height * .08,
-          width: size.width * .9,
-          child: ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).pushNamed('/quizPage');
-            },
-            child: Text(
-              "Teste Başla",
-              style: Theme.of(context)
-                  .textTheme
-                  .headline5
-                  .copyWith(color: Colors.white),
-            ),
-            style: ButtonStyle(
-              shape: MaterialStateProperty.all(StadiumBorder()),
+  AnimatedOpacity buildFooter(Size size) {
+    return AnimatedOpacity(
+      opacity: isDetailShowed ? 1 : 0,
+      duration: Duration(seconds: 1),
+      child: Visibility(
+        visible: isDetailShowed ? true : false,
+        child: Align(
+          alignment: Alignment.bottomCenter,
+          child: SizedBox(
+            height: size.height * .275,
+            child: Center(
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: SingleChildScrollView(
+                  child: Text(
+                    "Çocukların görsel algılarını değerlendirmeye "
+                    "yardımcı olan bir algı testidir. El-Göz "
+                    "Koordinasyonu, Şekil-Zemin Ayırımı, Algılama "
+                    "Sabitliği (Şekil Değişmezliği ), Mekânla Konumun"
+                    " Algılanması (Uzaydaki Pozisyon) ,Mekân "
+                    "İlişkilerinin (Uzay ilişkilerinin) Algılanması"
+                    " olmak üzere 5 algısal beceriyi ölçen bir"
+                    " performans testidir.",
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyText1
+                        .copyWith(color: Colors.white),
+                  ),
+                ),
+              ),
             ),
           ),
         ),
@@ -116,7 +179,7 @@ class _TestIntroductionViewState extends State<TestIntroductionView> {
     );
   }
 
-  Column buildBackground(Size size, BuildContext context) {
+  Column buildBackground(Size size) {
     return Column(
       children: [
         Expanded(
