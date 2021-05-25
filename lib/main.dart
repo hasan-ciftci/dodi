de9ec94979ad98d21aac949ee94eb4808279217e
hasan-ciftci <hasan.ciftci@ankabeta.com>
2021-05-25 13:40:08 +0300

    Textscaling restricted.

--- lib/main.dart
+++ lib/main.dart
@@ -31,61 +31,75 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-        designSize: Size(360, 690),
-        builder: () => MaterialApp(
-              title: 'Flutter Demo',
-              theme: ThemeData().copyWith(
-                toggleableActiveColor: Color(0xFFF38424),
-                primaryColor: Color(0xFF1C742C),
-                backgroundColor: Color(0xFFF38424),
-                accentColor: Color(0xFF1C742C),
-                elevatedButtonTheme: ElevatedButtonThemeData(
-                  style: ElevatedButton.styleFrom(
-                    onPrimary: Color(0xFFF38424),
-                    primary: Color(0xFFF38424),
-                  ),
-                ),
-                bottomAppBarColor: Color(0xFFE9F0D0),
-                textTheme: TextTheme(
-                  bodyText1: ThemeData().textTheme.bodyText1.copyWith(fontSize: 16.sp),
-                  bodyText2: ThemeData().textTheme.bodyText2.copyWith(fontSize: 14.sp),
-                  headline1: ThemeData().textTheme.headline1.copyWith(fontSize: 96.sp),
-                  headline2: ThemeData().textTheme.headline2.copyWith(fontSize: 60.sp),
-                  headline3: ThemeData().textTheme.headline3.copyWith(fontSize: 48.sp),
-                  headline4: ThemeData().textTheme.headline4.copyWith(fontSize: 34.sp),
-                  headline5: ThemeData().textTheme.headline5.copyWith(fontSize: 24.sp),
-                  headline6: ThemeData().textTheme.headline6.copyWith(fontSize: 20.sp),
-                  subtitle1: ThemeData().textTheme.subtitle1.copyWith(fontSize: 16.sp),
-                  subtitle2: ThemeData().textTheme.subtitle2.copyWith(fontSize: 14.sp),
-                  button: ThemeData().textTheme.button.copyWith(fontSize: 14.sp),
-                  caption: ThemeData().textTheme.caption.copyWith(fontSize: 12.sp),
-                  overline: ThemeData().textTheme.overline.copyWith(fontSize: 10.sp),
-                )
+      designSize: Size(360, 690),
+      builder: () => MaterialApp(
+        builder: (context, child) => MediaQuery(
+          child: child,
+          data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+        ),
+        title: 'Flutter Demo',
+        theme: ThemeData().copyWith(
+            toggleableActiveColor: Color(0xFFF38424),
+            primaryColor: Color(0xFF1C742C),
+            backgroundColor: Color(0xFFF38424),
+            accentColor: Color(0xFF1C742C),
+            elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+                onPrimary: Color(0xFFF38424),
+                primary: Color(0xFFF38424),
               ),
-              routes: {
-                '/a': (context) => IntroductionPage(),
-                '/loginView': (context) => LoginView(),
-                '/register': (context) => SignUpView(),
-                '/classSelectView': (context) => ClassSelectView(),
-                '/startTest': (context) => TestIntroductionView(),
-                '/questionView': (context) => QuizGradeOneView(),
-                '/quizGradeTwelveView': (context) => QuizGradeTwelveView(),
-                '/analyzingPage': (context) => AnalyzingView(),
-                '/resultPage': (context) => ResultView(),
-                '/evaluationOverview': (context) => EvaluationOverviewView(),
-                '/phoneRegister': (context) => PhoneRegisterView(),
-                '/chooseProfile': (context) => ChooseProfileView(),
-                '/createProfile': (context) => CreateProfileView(),
-                //Same with Choose Profile but included one more profile for scenerio.
-                '/chooseProfileExtra': (context) => ChooseProfileExtra(),
-                '/': (context) => CourseSelectView(),
-                '/subjectSelectView': (context) => SubjectSelectView(),
-                '/newClassSelectView': (context) => NewClassSelectView(),
-                '/solvedTestsView': (context) => SolvedTestsView(),
-                '/solutionsView': (context) => SolutionsView(),
-                '/solutionsGradeTwelveView': (context) =>
-                    SolutionsGradeTwelveView(),
-              },
-            ));
+            ),
+            bottomAppBarColor: Color(0xFFE9F0D0),
+            textTheme: TextTheme(
+              bodyText1:
+                  ThemeData().textTheme.bodyText1.copyWith(fontSize: 16.sp),
+              bodyText2:
+                  ThemeData().textTheme.bodyText2.copyWith(fontSize: 14.sp),
+              headline1:
+                  ThemeData().textTheme.headline1.copyWith(fontSize: 96.sp),
+              headline2:
+                  ThemeData().textTheme.headline2.copyWith(fontSize: 60.sp),
+              headline3:
+                  ThemeData().textTheme.headline3.copyWith(fontSize: 48.sp),
+              headline4:
+                  ThemeData().textTheme.headline4.copyWith(fontSize: 34.sp),
+              headline5:
+                  ThemeData().textTheme.headline5.copyWith(fontSize: 24.sp),
+              headline6:
+                  ThemeData().textTheme.headline6.copyWith(fontSize: 20.sp),
+              subtitle1:
+                  ThemeData().textTheme.subtitle1.copyWith(fontSize: 16.sp),
+              subtitle2:
+                  ThemeData().textTheme.subtitle2.copyWith(fontSize: 14.sp),
+              button: ThemeData().textTheme.button.copyWith(fontSize: 14.sp),
+              caption: ThemeData().textTheme.caption.copyWith(fontSize: 12.sp),
+              overline:
+                  ThemeData().textTheme.overline.copyWith(fontSize: 10.sp),
+            )),
+        routes: {
+          '/': (context) => IntroductionPage(),
+          '/loginView': (context) => LoginView(),
+          '/register': (context) => SignUpView(),
+          '/classSelectView': (context) => ClassSelectView(),
+          '/startTest': (context) => TestIntroductionView(),
+          '/questionView': (context) => QuizGradeOneView(),
+          '/quizGradeTwelveView': (context) => QuizGradeTwelveView(),
+          '/analyzingPage': (context) => AnalyzingView(),
+          '/resultPage': (context) => ResultView(),
+          '/evaluationOverview': (context) => EvaluationOverviewView(),
+          '/phoneRegister': (context) => PhoneRegisterView(),
+          '/chooseProfile': (context) => ChooseProfileView(),
+          '/createProfile': (context) => CreateProfileView(),
+          //Same with Choose Profile but included one more profile for scenerio.
+          '/chooseProfileExtra': (context) => ChooseProfileExtra(),
+          '/courseSelectView': (context) => CourseSelectView(),
+          '/subjectSelectView': (context) => SubjectSelectView(),
+          '/newClassSelectView': (context) => NewClassSelectView(),
+          '/solvedTestsView': (context) => SolvedTestsView(),
+          '/solutionsView': (context) => SolutionsView(),
+          '/solutionsGradeTwelveView': (context) => SolutionsGradeTwelveView(),
+        },
+      ),
+    );
   }
 }
